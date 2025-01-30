ff9aeddb0c5500ebac6eb08e4a2787e9d4bfde1a
OrlandoDev17 <orlandodev1707@gmail.com>
2025-01-30 14:09:29 -0400

    add catalogue section to proyect

---
+++ src/data/catalogue.ts
@@ -0,0 +1,105 @@
+interface shoe {
+  id: string;
+  name: string;
+  price: number;
+  maker: string;
+  images: string[];
+}
+
+export const catalogue: shoe[] = [
+  {
+    id: 'Air-Jordan-1-Retro-Low-OG-SP-Travis-Scott-Medium-Olive',
+    name: 'Air Jordan 1 Retro Low OG SP Travis Scott Medium Olive',
+    price: 700,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/Jordan1RetroLowOGSP_TravisScottMediumOlive.png?v=1726833258&width=700',
+      'https://ciconceptstore.com/cdn/shop/files/0001_AIR-JORDAN-1-RETRO-LOW-OG-SP-TRAVIS-SCOTT-MEDIUM-OLIVE.png?v=1726833258&width=700',
+    ],
+  },
+  {
+    id: 'Adidas-Campus-x-Bad-Bunny-The-Last-Campus',
+    name: 'Adidas Campus x Bad Bunny - The Last Campus',
+    price: 230,
+    maker: 'Adidas',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/Adidas_Campus_x_Bad-Bunny_The_Last_Campus1.webp?v=1719060145&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Adidas_Campus_x_Bad-Bunny_The_Last_Campus2.webp?v=1719060145&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Copia-de-Diseno-sin-titulo-_1.webp?v=1719060419&width=800',
+    ],
+  },
+  {
+    id: 'Air-Jordan-4-x-Ma-Maniere-Retro-WMNS',
+    name: 'Air Jordan 4 x A Ma Maniére Retro WMNS - While You Were Sleeping',
+    price: 350,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/FZ4810-200.png?v=1727422298&width=1000',
+      'https://ciconceptstore.com/cdn/shop/files/FZ4810-200_2.jpg?v=1727422298&width=1000',
+    ],
+  },
+  {
+    id: 'Air-Jordan-Jumpman-Jack-TR-Travis-Scott-Dark-Mocha',
+    name: 'Air Jordan Jumpman Jack TR Travis Scott Dark Mocha',
+    price: 550,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/01_397f3ba7-d7f7-41f6-9280-25c4009ad772.png?v=1731335270&width=700',
+      'https://ciconceptstore.com/cdn/shop/files/02_9ceebeee-04ab-48da-bd64-315d34599e48.png?v=1731335270&width=700',
+    ],
+  },
+  {
+    id: 'Adidas-Handball-Spezial-Earth-Strata-Gum',
+    name: 'Adidas Handball Spezial Earth Strata Gum',
+    price: 180,
+    maker: 'Adidas',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/Adidas_Handball_Spezial_Earth_Strata_Gum.webp?v=1719062364&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Adidas_Handball_Spezial_Earth_Strata_Gum1.webp?v=1719062364&width=500',
+    ],
+  },
+  {
+    id: 'Nike-Dunk-Low-Grey-Fog',
+    name: 'Nike Dunk Low - Grey Fog',
+    price: 200,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/products/1_10_82280888-8014-4305-8c3e-44b635f22026.png?v=1676122227&width=1000',
+      'https://ciconceptstore.com/cdn/shop/products/2_9_d8d58906-2a64-40b9-a349-70b11bd40b4a.png?v=1676122232&width=1000',
+      'https://ciconceptstore.com/cdn/shop/products/3_7_cccd53e7-8b64-4a66-8ed0-54c328cd13d8.png?v=1676122235&width=1000',
+    ],
+  },
+  {
+    id: 'Adidas-Forum-x-Bad-Bunny-Buckle-Low-White',
+    name: 'Adidas Forum x Bad Bunny - Buckle Low White',
+    price: 330,
+    maker: 'Adidas',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/1_2.webp?v=1688729153&width=1000',
+      'https://ciconceptstore.com/cdn/shop/files/6_2c519c4f-ca7c-496c-9395-5c5b21b4e723.webp?v=1718971036&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Copia-de-Diseno-sin-titulo-_4.webp?v=1719061725&width=800',
+    ],
+  },
+  {
+    id: 'Air-Force-1-Low-NOCTA-Drake_Certified-Lover-Boy',
+    name: 'Air Force 1 Low NOCTA Drake Certified Lover Boy',
+    price: 250,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/Air_Force_1_Low_NOCTA_Drake_Certified_Lover_Boy1.webp?v=1738154163&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Air_Force_1_Low_NOCTA_Drake_Certified_Lover_Boy2.webp?v=1738154163&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Air_Force_1_Low_NOCTA_Drake_Certified_Lover_Boy3.webp?v=1738154163&width=800',
+    ],
+  },
+  {
+    id: 'Nike-SB-Dunk-Low_Rayssa-Leal',
+    name: 'Nike SB Dunk Low Rayssa Leal',
+    price: 250,
+    maker: 'Nike',
+    images: [
+      'https://ciconceptstore.com/cdn/shop/files/Nike_SB_Dunk_Low_Rayssa_Leal3.webp?v=1719658004&width=800',
+      'https://ciconceptstore.com/cdn/shop/files/Nike_SB_Dunk_Low_Rayssa_Leal2.webp?v=1719658004&width=500',
+      'https://ciconceptstore.com/cdn/shop/files/Nike_SB_Dunk_Low_Rayssa_Leal1.webp?v=1719658004&width=500',
+    ],
+  },
+];
